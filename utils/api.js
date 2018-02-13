1322511e4f8409e53efa89a51389ac9f42f28be3
E.S <extrin13@gmail.com>
2018-02-13 23:03:15 +0300

    feat: Add API methods

---
+++ utils/api.js
@@ -0,0 +1,34 @@
+import { AsyncStorage } from 'react-native';
+
+const CARDS_STORAGE_KEY = 'MobileFlashcards:decks';
+
+export const addCardToDeck = (deckTitle, question, answer) => {
+  const deck = getDeck(deckTitle);
+  const updatedQuestions = deck.questions.concat([
+    {
+      question: question,
+      answer: answer
+    }
+  ]);
+  AsyncStorage.mergeItem(
+    CARDS_STORAGE_KEY,
+    JSON.stringify({
+      [deckTitle]: { title: deckTitle, questions: updatedQuestions }
+    })
+  );
+};
+
+export const saveDeckTitle = deckTitle =>
+  AsyncStorage.mergeItem(
+    CARDS_STORAGE_KEY,
+    JSON.stringify({ [deckTitle]: { title: deckTitle, questions: [] } })
+  );
+
+export const getDeck = key =>
+  AsyncStorage.getItem(CARDS_STORAGE_KEY).then(res => {
+    const decks = JSON.parse(res);
+    return decks[key];
+  });
+
+export const getAllDecks = () =>
+  AsyncStorage.getItem(CARDS_STORAGE_KEY).then(res => JSON.parse(res));
