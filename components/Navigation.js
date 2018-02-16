ec8ae5c1176a934b319e9c3af916d1607234f143
E.S <extrin13@gmail.com>
2018-02-16 23:48:07 +0300

    feat: Create navigation component

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,1 +1,29 @@
-//TODO: Create navigation component
+import React from 'react';
+import { TabNavigator } from 'react-navigation';
+import DeckListView from './DeckListView';
+import NewDeckView from './NewDeckView';
+import { light_primary_color, accent_color } from '../utils/colors';
+
+const TabNav = TabNavigator(
+  {
+    DeckListView: {
+      screen: DeckListView,
+      navigationOptions: {
+        tabBarLabel: 'DECKS'
+      }
+    },
+    NewDeckView: {
+      screen: NewDeckView,
+      navigationOptions: {
+        tabBarLabel: 'NEW DECK'
+      }
+    }
+  },
+  {
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: { activeTintColor: accent_color },
+    style: { backgroundColor: light_primary_color }
+  }
+);
