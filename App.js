df050d6b55be1ae74ed4f33e66b6a67abff49dba
E.S <extrin13@gmail.com>
2018-02-18 00:05:17 +0300

    feat: Render something

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MyStatusBar from './components/MyStatusBar';
+import { TabNav } from './components/Navigation';
+import { light_primary_color } from './utils/colors';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1 }}>
+        <MyStatusBar
+          backgroundColor={light_primary_color}
+          barStyle="light-content"
+        />
+        <TabNav />
       </View>
     );
   }
@@ -18,6 +23,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
