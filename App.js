d19ee032ba3804c1b9448c5afc749dca7b094a10
E.S <extrin13@gmail.com>
2018-03-04 15:38:06 +0300

    feat: Add store provider and create store

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import MyStatusBar from './components/MyStatusBar';
 import { MainNavigator } from './components/Navigation';
 import { light_primary_color } from './utils/colors';
 import { setLocalNotification } from './utils/helpers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { decks } from './reducers';
 
 export default class App extends React.Component {
   componentDidMount() {
@@ -12,13 +15,15 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <MyStatusBar
-          backgroundColor={light_primary_color}
-          barStyle="light-content"
-        />
-        <MainNavigator />
-      </View>
+      <Provider store={createStore(decks)}>
+        <View style={styles.container}>
+          <MyStatusBar
+            backgroundColor={light_primary_color}
+            barStyle="light-content"
+          />
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
